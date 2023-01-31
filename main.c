ac237dd69891782eac853e868f63bb62168f087f
Facundo <facundo.troitero@fedrax.com>
2023-01-31 17:15:39 +0000

    added libft submodule

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "pipex.h"
+
+void main()
+{
+	int pfret;
+	int ftpfret;
+
+	void *ptr = (void *)NULL;
+	pfret = printf("%x\n", 42);
+	ftpfret = ft_printf("%x\n", 42);
+	printf("%d\n", pfret);
+	printf("%d\n", ftpfret);
+
+}
--- pipex.h
+++ pipex.h
@@ -4,7 +4,7 @@
 # include <fcntl.h> // open
 # include <unistd.h> // close, read, write, access, dup, dup2, execve, fork, pipe
 # include <stdlib.h> // malloc, exit
-# include <stdio.h> // perror
+# include <stdio.h> // perror, printf
 # include <string.h> // strerror
 #include <sys/types.h> // fork, wait
 #include <sys/wait.h>  // wait, waitpid
