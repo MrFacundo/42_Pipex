e7902b0283339492a8ea64a6deb46a6a84a2d273
Facundo <facundo.troitero@fedrax.com>
2023-02-14 16:08:04 +0000

    tests

--- srcs/utils.c
+++ srcs/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ftroiter <ftroiter@student.42.fr>          +#+  +:+       +#+        */
+/*   By: facundo <facundo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 13:03:55 by ftroiter          #+#    #+#             */
-/*   Updated: 2023/02/12 13:35:25 by ftroiter         ###   ########.fr       */
+/*   Updated: 2023/02/14 16:06:40 by facundo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void	error(char *err)
 {
 	perror(err);
-	exit (1);
+	exit(1);
 }
 
 void	close_pipe_ends(int pipe_fd[])
@@ -25,24 +25,99 @@ void	close_pipe_ends(int pipe_fd[])
 	close(pipe_fd[1]);
 }
 
-char	**parse_process_string(char	*process_string)
+static	size_t pipex_count_tokens(char const *s, char c)
 {
-	char	**argv;
-	int		i;
+	size_t count;
 
-	argv = ft_split(process_string, ' ');
+	count = 0;
+	while (*s)
+	{
+		if (*s == c)
+			s++;
+		else
+		{
+			if (*s == '\'' && ++count)
+			{
+				s++;
+				while (*s && *s != '\'')
+					s++;
+				if (*s == '\'')
+					s++;
+			}
+			else
+			{
+				count++;
+				while (*s && *s != c)
+					s++;
+			}
+		}
+	}
+	return (count);
+}
+
+char **ft_pipex_split(char const *s, char c)
+{
+	char **result;
+	size_t i;
+	size_t len;
+
+	i = 0;
+	result = malloc(sizeof(char *) * (pipex_count_tokens(s, c) + 1));
+	if (!result || !s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == c)
+			s++;
+		else
+		{
+			if (*s && *s == '\'')
+			{
+				while (*s == '\'')
+					s++;
+				len = 2;
+				while (*s && *s != '\'' && ++len)
+					++s;
+				if (*s == '\'')
+					++s;
+				result[i++] = ft_substr(s - len, 0, len);
+			}
+			if (*s && *s != '\'')
+			{
+				len = 0;
+				while (*s && *s != c && ++len)
+					++s;
+				result[i++] = ft_substr(s - len, 0, len);
+			}
+		}
+	}
+	result[i] = 0;
+	return (result);
+}
+
+char	**parse_process_string(char *process_string)
+{
+	char **argv;
+	int i;
+	char *temp;
+
+	argv = ft_pipex_split(process_string, ' ');
 	i = 0;
 	while (argv[i])
+	{
+		temp = ft_strtrim(argv[i], "\"\'");
+		ft_strlcpy(argv[i], temp, ft_strlen(temp) + 1);
 		i++;
-	argv[i] = 0;
+		free(temp);
+	}
 	return (argv);
 }
 
 char	*get_path(char *envp[], char *process)
 {
-	char	**paths;
-	char	*path_string;
-	char	*path;
+	char **paths;
+	char *path_string;
+	char *path;
 
 	while (*envp && !ft_strnstr(*envp, "PATH=", 5))
 		envp++;
@@ -58,7 +133,7 @@ char	*get_path(char *envp[], char *process)
 		if (!path)
 			return (0);
 		if (!access(path, F_OK))
-			break ;
+			break;
 		free(path);
 		paths++;
 	}
@@ -66,3 +141,4 @@ char	*get_path(char *envp[], char *process)
 		error("ERR_CMD");
 	return (path);
 }
+
